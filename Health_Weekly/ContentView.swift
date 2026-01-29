f379ef6c493021be63936167671ef6389a280736
ricpinna <errepinna@gmail.com>
2026-01-29 08:45:23 +0100

    Initial Commit

---
+++ Health_Weekly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Health_Weekly
+//
+//  Created by Riccardo Pinna on 29/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Health_Weekly/Health_WeeklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_WeeklyApp.swift
+//  Health_Weekly
+//
+//  Created by Riccardo Pinna on 29/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Health_WeeklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
